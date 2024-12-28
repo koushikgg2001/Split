15078a03b1fc434fb0708e5223cb6c010165ba4a
Varshini B S <varshinibs01@gmail.com>
2024-12-28 20:19:06 +0530

    SPLT-8 create controllers and routes for login and registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,15 @@
+
+import User from "../models/userSchema.js";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+//validation for email and passwords
+function validateEmail(email){
+  const emailPattern =  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  return emailPattern.test(email);
+}
 // Controller to handle fetching all users
 const getAllUsers = (req, res) => {
     res.json([{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Smith' }]);
@@ -8,5 +20,83 @@ const getUserById = (req, res) => {
   const userId = req.params.id;
   res.json({ id: userId, name: `User ${userId}` });
 };
+
+const registerUser = async (req,res)=>{
+  try {
+      const email = req.body.email;
+      const name = req.body.name;
+      const password = req.body.password;
+      console.log(email,password,name);
+      if (!name || !email || !password) {
+        return res.status(400).json({ message: "All fields are required." });
+      }
+
+      if(!validateEmail(email))
+      {
+        return res.status(400).json({ message: "not a valid email." });
+      }
   
-export default { getAllUsers, getUserById };
+      const checkResult = await User.findOne({email:email});
+  
+      if (checkResult) {
+        res.send("Email already exists. Try logging in.");
+      } else {
+        //hashing the password and saving it in the database
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+          if (err) {
+            console.error("Error hashing password:", err);
+          } else {
+            console.log("Hashed Password:", hash);
+            const id = await User.create({
+              name : name,
+              email : email,
+              password: hash
+            });
+            console.log(`id  -- ${id}`);
+            res.send("registered successfully");
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+const loginUser = async (req,res)=>{
+  try {
+    const email = req.body.email;
+    const loginpassword = req.body.password;
+    console.log(email,loginpassword);
+    if ( !email || !loginpassword) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    if(!validateEmail(email))
+    {
+      return res.status(400).json({ message: "not a valid email." });
+    }
+    const  user = await User.findOne({email: email});
+    if (!user)
+    {
+      return res.status(400).json({ message: "Email not registered yet. Please reigster" });
+    }
+    else{
+      const validCred = await bcrypt.compare(loginpassword, user.password);
+      if (!validCred)
+      {
+        return res.status(400).json({ message: "Wrong password" });
+      }
+      else{
+        return res.status(200).json({ message: "login successful" });
+      }
+    }
+    
+    
+  }catch(err)
+  {
+    console.log(err);
+  }
+
+}
+  
+export default { getAllUsers, getUserById, registerUser, loginUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import userController from '../controllers/userController.js';
-const { getAllUsers, getUserById } = userController;
+const { getAllUsers, getUserById, registerUser, loginUser } = userController;
 
 // Define routes
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
+router.post('/register',registerUser);
+router.post('/login',loginUser);
 
 export default router;
