f78470ebe82d5e6f2b87ab8e40028f0e7f7548d3
Koushik G G <koushikgg2001@gmail.com>
2025-01-12 19:45:02 +0530

    SPLT-6 Add Signup component

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import bodyParser from "body-parser";
 import cors from 'cors';
 import userController from '../controllers/userController.js';
-const { getAllUsers, getUserById, registerUser, loginUser } = userController;
+
+const router = express.Router();
+const {registerUser, loginUser } = userController;
+
+router.use(cors({
+  origin: 'http://localhost:3000'
+}));
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json()); 
 
 // Define routes
-router.get('/', getAllUsers);
-router.get('/:id', getUserById);
 router.post('/register',registerUser);
 router.post('/login',loginUser);
 
